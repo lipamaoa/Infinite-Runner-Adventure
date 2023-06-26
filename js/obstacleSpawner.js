4ae64129b87202d1e2361f2c7fbee0b88c2e8bc3
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-06-27 00:30:36 +0100

    Added getRandomSpawnInterval

--- js/obstacleSpawner.js
+++ js/obstacleSpawner.js
@@ -1,40 +1,45 @@
 class ObstacleSpawner {
     constructor(gameScreen) {
-      this.gameScreen = gameScreen;
-      this.obstacles = [];
-      this.spawnInterval = 2000; 
-      this.lastSpawnTime = Date.now();
+        this.gameScreen = gameScreen;
+        this.obstacles = [];
+        this.minSpawnInterval = 2000;
+        this.maxSpawnInterval = 4000;
+        this.nextSpawnTime = Date.now() + this.getRandomSpawnInterval();
     }
-  
+
     update(speed, player, collisionCallback) {
-      // Check if it's time to spawn a new obstacle
-      if (Date.now() - this.lastSpawnTime >= this.spawnInterval) {
-        this.spawnObstacle();
-        this.lastSpawnTime = Date.now();
-      }
-
-      // Update all obstacles
-      for (let i = this.obstacles.length - 1; i >= 0; i--) {
-        const obstacle = this.obstacles[i];
-        obstacle.move(speed);
-  
-        const collidedWithPlayer = obstacle.collidedWithPlayer(player);
-
-        // Remove the obstacle if it's off the screen or collided with the player
-        if (obstacle.isOutOfScreen() || collidedWithPlayer) {
-          this.gameScreen.removeChild(obstacle.element);
-          this.obstacles.splice(i, 1);
+        // Check if it's time to spawn a new obstacle
+        if (Date.now() > this.nextSpawnTime) {
+            this.spawnObstacle();
+            this.nextSpawnTime = Date.now() + this.getRandomSpawnInterval();
         }
 
-        if (collidedWithPlayer){
-            collisionCallback();
+        // Update all obstacles
+        for (let i = this.obstacles.length - 1; i >= 0; i--) {
+            const obstacle = this.obstacles[i];
+            obstacle.move(speed);
+
+            const collidedWithPlayer = obstacle.collidedWithPlayer(player);
+
+            // Remove the obstacle if it's off the screen or collided with the player
+            if (obstacle.isOutOfScreen() || collidedWithPlayer) {
+                this.gameScreen.removeChild(obstacle.element);
+                this.obstacles.splice(i, 1);
+            }
+
+            if (collidedWithPlayer) {
+                collisionCallback();
+            }
         }
-      }
     }
-  
+
     spawnObstacle() {
         const obstacleStyle = Math.ceil(Math.random() * 3);
-      const obstacle = new Obstacle(this.gameScreen, 150, `obstacle-type-${obstacleStyle}`);
-      this.obstacles.push(obstacle);
+        const obstacle = new Obstacle(this.gameScreen, 150, `obstacle-type-${obstacleStyle}`);
+        this.obstacles.push(obstacle);
     }
-  }
+
+    getRandomSpawnInterval() {
+        return Math.random() * (this.maxSpawnInterval - this.minSpawnInterval) + this.minSpawnInterval;
+    }
+}
